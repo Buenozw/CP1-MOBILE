429b520dfa2fc64b97bb21f2290a2e057ef25f4e
Buenozw <jvictorcastelini2006@gmail.com>
2026-04-13 17:21:03 -0300

    feat:"Criando a navegação da pg"

--- CP01MOBILE/App.js
+++ CP01MOBILE/App.js
@@ -2,27 +2,11 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native-web";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import routes from './src/routes'
-import CadastroScreen from "./screens/CadastroScreen";
-import PerfilScreen from "./screens/PerfilScreen";
+import routes from './routes'
+import AppNavigator from "./src/navigation/AppNavigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Cadastro">
-        <Stack.Screen
-          name="Cadastro"
-          component={CadastroScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Perfil"
-          component={PerfilScreen}
-          options={{ title: "Perfil do Aluno" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 }
---
+++ CP01MOBILE/src/sreens/Navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import CadastroScreen from "../screens/CadastroScreen";
+import PerfilScreen from "../screens/PerfilScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Cadastro" component={CadastroScreen} />
+        <Stack.Screen name="Perfil" component={PerfilScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
